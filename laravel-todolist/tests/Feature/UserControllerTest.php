ef29c0aad48f78129855d4c4dea4bdfebec13989
adenafil <nafilie9@gmail.com>
2024-04-07 01:57:36 +0700

    57. Membuat Middleware Member

--- laravel-todolist/tests/Feature/UserControllerTest.php
+++ laravel-todolist/tests/Feature/UserControllerTest.php
@@ -64,4 +64,10 @@ class UserControllerTest extends TestCase
             ->assertRedirect('/')
             ->assertSessionMissing('user');
     }
+
+    public function testLogoutGuest()
+    {
+        $this->post('/logout')
+            ->assertRedirect('/');
+    }
 }
