6d4c76e1bab7fa324eeb6238f88ffcacd998ba73
adenafil <nafilie9@gmail.com>
2024-04-06 14:51:31 +0700

    41. Error Handling

---
+++ laravel/belajar-laravel-dasar/app/Exceptions/ValidationException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Exceptions;
+
+class ValidationException extends \Exception
+{
+    public function __construct(string $message)
+    {
+        parent::__construct($message);
+    }
+}
--- laravel/belajar-laravel-dasar/routes/web.php
+++ laravel/belajar-laravel-dasar/routes/web.php
@@ -147,3 +147,16 @@ Route::get('/url/current', function () {
 
 Route::get('/session/create', [\App\Http\Controllers\SessionController::class, 'createSession']);
 Route::get('/session/get', [\App\Http\Controllers\SessionController::class, 'getSession']);
+
+Route::get('/error/sample', function () {
+    throw new Exception('Sample Error');
+});
+
+Route::get('/error/manual', function () {
+   report(new Exception('Sample Error'));
+   return 'OK';
+});
+
+Route::get('/error/validation', function () {
+   throw new \App\Exceptions\ValidationException('Validation Error');
+});
