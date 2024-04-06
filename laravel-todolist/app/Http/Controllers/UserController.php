ca94f323ac58a75a9c57cd778e2c3496ad9fcd89
adenafil <nafilie9@gmail.com>
2024-04-07 00:35:16 +0700

    53. Membuat Login Page

--- laravel-todolist/app/Http/Controllers/UserController.php
+++ laravel-todolist/app/Http/Controllers/UserController.php
@@ -8,7 +8,10 @@ class UserController extends Controller
 {
     public function login()
     {
-
+        return response()
+            ->view('user.login', [
+                'title' => 'Login'
+            ]);
     }
 
     public function doLogin()
