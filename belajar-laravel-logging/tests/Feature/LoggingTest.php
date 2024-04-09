b0b876c2eed018c7da098b53fbff197dac731d12
adenafil <nafilie9@gmail.com>
2024-04-09 20:29:06 +0700

    76. Context

--- belajar-laravel-logging/tests/Feature/LoggingTest.php
+++ belajar-laravel-logging/tests/Feature/LoggingTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
+use function PHPUnit\Framework\assertTrue;
 
 class LoggingTest extends TestCase
 {
@@ -17,5 +18,31 @@ class LoggingTest extends TestCase
         Log::critical('Hello Critical');
 
         self::assertTrue(true);
+    }
+
+    public function testContext()
+    {
+        Log::info('Hello Info', [
+            'user' => 'ade'
+        ]);
+        Log::info('Hello Info', [
+            'user' => 'ade'
+        ]);
+        Log::info('Hello Info', [
+            'user' => 'ade'
+        ]);
+
+        assertTrue(true);
+    }
+
+    public function testWithContext()
+    {
+        Log::withContext(['user' => 'ade']);
+
+        Log::info('Hello Info');
+        Log::info('Hello Info');
+        Log::info('Hello Info');
+
+        assertTrue(true);
     }
 }
