1b1aedc41cd39a2187e00fe5322cdac07f9b8fb2
adenafil <nafilie9@gmail.com>
2024-04-10 17:00:42 +0700

    93. Switch Statement

---
+++ belajar-laravel-blade-template/resources/views/switch.blade.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+@switch($value)
+    @case('A')
+        Memuaskan
+        @break
+    @case('B')
+        Bagus
+        @break
+    @case('C')
+        Cukup
+        @break
+    @default
+        Tidak Lulus
+@endswitch
+</body>
+</html>
