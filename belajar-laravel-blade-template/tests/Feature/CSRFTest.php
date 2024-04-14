91976985b091da25335b6d4f558357cbc3de244c
adenafil <nafilie9@gmail.com>
2024-04-15 06:28:40 +0700

    103. CSRF

---
+++ belajar-laravel-blade-template/tests/Feature/CSRFTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CSRFTest extends TestCase
+{
+    public function testCSRF()
+    {
+        $this->view('csrf', [])
+            ->assertSee('hidden')
+            ->assertSee('_token');
+    }
+}
