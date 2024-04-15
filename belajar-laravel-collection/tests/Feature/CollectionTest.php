ddbf53107c5e0b1929f8777e3b1f359b09ac6223
adenafil <nafilie9@gmail.com>
2024-04-15 10:15:57 +0700

    117. Membuat Collection

---
+++ belajar-laravel-collection/tests/Feature/CollectionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CollectionTest extends TestCase
+{
+    public function testCreateCollection()
+    {
+        $collection = collect([1, 2, 3]);
+        self::assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+    }
+}
