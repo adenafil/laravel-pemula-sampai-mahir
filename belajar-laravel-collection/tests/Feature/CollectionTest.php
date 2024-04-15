622c0898c1b7df8192977deaaf2ca64a482dd089
adenafil <nafilie9@gmail.com>
2024-04-15 10:31:15 +0700

    118. ForEach

--- belajar-laravel-collection/tests/Feature/CollectionTest.php
+++ belajar-laravel-collection/tests/Feature/CollectionTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use function PHPUnit\Framework\assertEquals;
 
 class CollectionTest extends TestCase
 {
@@ -12,5 +13,13 @@ class CollectionTest extends TestCase
     {
         $collection = collect([1, 2, 3]);
         self::assertEqualsCanonicalizing([1, 2, 3], $collection->all());
+    }
+
+    public function testForEach()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        foreach ($collection as $key => $value) {
+            assertEquals($key + 1, $value);
+        }
     }
 }
