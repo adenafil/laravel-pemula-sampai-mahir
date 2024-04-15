6611c1621bb63e3dada0d4d65c38f250c4d780f4
adenafil <nafilie9@gmail.com>
2024-04-15 09:19:11 +0700

    109. Extending Blade Template

---
+++ belajar-laravel-blade-template/tests/Feature/ExtendingTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ExtendingTest extends TestCase
+{
+    public function testExtending()
+    {
+        $this->view("extending", ["name" => "ade"]
+        )->assertSeeText('Hello ade');
+    }
+}
