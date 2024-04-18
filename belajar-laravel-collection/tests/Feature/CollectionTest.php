8e3965fb885a64320d54f46425dbd3a99cf435c8
adenafil <nafilie9@gmail.com>
2024-04-18 15:31:40 +0700

    128. Slicing

--- belajar-laravel-collection/tests/Feature/CollectionTest.php
+++ belajar-laravel-collection/tests/Feature/CollectionTest.php
@@ -343,6 +343,27 @@ class CollectionTest extends TestCase
 
     }
 
+    public function testSlice()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
+        $result = $collection->slice(3);
+        assertEquals([
+            3 => 4,
+            4 => 5,
+            5 => 6,
+            6 => 7,
+            7 => 8,
+            8 => 9,
+        ], $result->all());
 
+        assertEqualsCanonicalizing([4, 5, 6, 7, 8, 9], $result->all());
+
+        $result = $collection->slice(3, 2);
+        assertEquals([
+            3 => 4,
+            4 => 5,
+        ], $result->all());
+        assertEqualsCanonicalizing([4, 5], $result->all());
+    }
 }
