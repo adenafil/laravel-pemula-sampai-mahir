568de8abacaf116bbfda3cea713da99787cb49c1
adenafil <nafilie9@gmail.com>
2024-04-19 15:39:01 +0700

    145. Membuat Database

--- belajar-laravel-database/database.sql
+++ belajar-laravel-database/database.sql
@@ -1,3 +1,13 @@
 create database belajar_laravel_database;
 
 use belajar_laravel_database;
+
+create table cetagories
+(
+    id varchar(100) not null primary key ,
+    name varchar(100) not null ,
+    description text,
+    created_at timestamp
+) engine  = innodb;
+
+desc cetagories;
