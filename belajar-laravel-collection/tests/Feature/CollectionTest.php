e8449ee56cb54f1c30046769da4db047c10d267b
adenafil <nafilie9@gmail.com>
2024-04-19 14:39:31 +0700

    138. Lazy Collection

--- belajar-laravel-collection/tests/Feature/CollectionTest.php
+++ belajar-laravel-collection/tests/Feature/CollectionTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Data\Person;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\LazyCollection;
 use Tests\TestCase;
 use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertEqualsCanonicalizing;
@@ -514,5 +515,23 @@ class CollectionTest extends TestCase
         });
 
         assertEquals(45, $result);
+    }
+
+    public function testLazyCollection()
+    {
+        $collection = LazyCollection::make(function () {
+
+            $value = 0;
+
+            while(true) {
+                yield $value;
+                $value++;
+            }
+
+        });
+
+        $result = $collection->take(10);
+
+        assertEquals([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], $result->all());
     }
 }
