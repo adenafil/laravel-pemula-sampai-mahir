3eccc6ff9cd2cab11beeb715ebe6290a70c29c1d
adenafil <nafilie9@gmail.com>
2024-04-19 10:39:03 +0700

    131. Retrieve

--- belajar-laravel-collection/tests/Feature/CollectionTest.php
+++ belajar-laravel-collection/tests/Feature/CollectionTest.php
@@ -422,4 +422,36 @@ class CollectionTest extends TestCase
         assertEqualsCanonicalizing([7, 8, 9], $result->all()[2]->all());
         assertEqualsCanonicalizing([10], $result->all()[3]->all());
     }
+
+    public function testFirst()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        $result = $collection->first();
+
+        assertEquals(1, $result);
+
+        $result = $collection->first(function ($value, $key) {
+            return $value > 5;
+        });
+
+        assertEquals(6, $result);
+
+    }
+
+    public function testLast()
+    {
+        $collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        $result = $collection->last();
+
+        assertEquals(9, $result);
+
+        $result = $collection->last(function ($value, $key) {
+            return $value < 5;
+        });
+
+        assertEquals(4, $result);
+
+    }
 }
