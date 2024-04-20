8650d2d3b33fdd9459ea21a0d611298795c463f7
adenafil <nafilie9@gmail.com>
2024-04-20 11:16:49 +0700

    157. Query Builder Join

--- belajar-laravel-database/database.sql
+++ belajar-laravel-database/database.sql
@@ -20,3 +20,21 @@ create table counters(
 insert into counters(id) values ('sample');
 
 select * from counters;
+
+create table products
+(
+    id varchar(100) not null primary key ,
+    name varchar(100) not null ,
+    description text null,
+    price int not null ,
+    category_id varchar(100) not null ,
+    created_at timestamp not null default current_timestamp,
+    constraint fk_category_id foreign key (category_id) references categories (id)
+) engine Innodb;
+
+desc products;
+
+select *
+from products;
+
+
