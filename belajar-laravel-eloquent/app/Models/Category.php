0772b65b4e11357718dd885e7358e78b5e01f733
adenafil <nafilie9@gmail.com>
2024-04-22 21:31:35 +0700

    180. Insert

--- belajar-laravel-eloquent/app/Models/Category.php
+++ belajar-laravel-eloquent/app/Models/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $table = 'categoires';
+    protected $table = 'categories';
     protected $primaryKey = 'id';
     protected $keyType = 'string';
     public $incrementing = false;
