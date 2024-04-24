5e53dca9ef739d1d0c8dc3d898e6ddbafbb26f76
adenafil <nafilie9@gmail.com>
2024-04-24 14:37:44 +0700

    195. Query Global Scope

--- belajar-laravel-eloquent/tests/Feature/CategoryTest.php
+++ belajar-laravel-eloquent/tests/Feature/CategoryTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use App\Models\Scopes\IsActiveScope;
 use Database\Seeders\CategorySeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -185,5 +186,22 @@ class CategoryTest extends TestCase
         $category->save();
 
         assertNotNull($category->id);
+    }
+
+    public function testGlobalScope()
+    {
+        $category = new Category();
+        $category->id = 'FOOD';
+        $category->name = 'Food';
+        $category->description = 'Food Category';
+        $category->is_active = false;
+        $category->save();
+
+        $category = Category::query()->find('FOOD');
+        self::assertNull($category);
+
+        $category = Category::query()->withoutGlobalScope(IsActiveScope::class)->find('FOOD');
+        self::assertNotNull($category);
+
     }
 }
