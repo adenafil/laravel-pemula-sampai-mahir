b0351f9dcdd106a27bd4e9587359ab3a567a1a51
adenafil <nafilie9@gmail.com>
2024-04-24 10:03:03 +0700

    189. UUID

---
+++ belajar-laravel-eloquent/tests/Feature/VoucherTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Voucher;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class VoucherTest extends TestCase
+{
+    public function testCreateVoucher()
+    {
+        $voucher = new Voucher();
+        $voucher->name = 'Sample Voucher';
+        $voucher->voucher_code = '082149250644';
+        $voucher->save();
+
+        self::assertNotNull($voucher->id);
+    }
+
+    public function testCreateVoucherUUID()
+    {
+        $voucher = new Voucher();
+        $voucher->name = 'Sample Voucher';
+        $voucher->save();
+
+        self::assertNotNull($voucher->id);
+        self::assertNotNull($voucher->voucher_code);
+    }
+
+}
