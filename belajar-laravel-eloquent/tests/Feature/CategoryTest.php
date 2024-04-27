d4ff44ec1d880f08ee18e492693cc3d7cff5f6cf
adenafil <nafilie9@gmail.com>
2024-04-27 15:37:05 +0700

    200. Query Builder Relationship

--- belajar-laravel-eloquent/tests/Feature/CategoryTest.php
+++ belajar-laravel-eloquent/tests/Feature/CategoryTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\Scopes\IsActiveScope;
 use Database\Seeders\CategorySeeder;
 use Database\Seeders\ProductSeeder;
@@ -219,4 +220,53 @@ class CategoryTest extends TestCase
         $total = $products->count();
         assertEquals(1, $total);
     }
+
+    public function testOneToManyQuery()
+    {
+        $category = new Category();
+        $category->id = 'FOOD';
+        $category->name = 'Food';
+        $category->description = 'Food Category';
+        $category->is_active = true;
+        $category->save();
+
+        $product = new Product();
+        $product->id = '1';
+        $product->name = 'Product 1';
+        $product->description = 'Description 1';
+
+        $category->products()->save($product);
+
+        self::assertNotNull($product->category_id);
+    }
+
+    public function testOneToManyQueryWithSeeder()
+    {
+        $this->seed(CategorySeeder::class);
+        $category = Category::query()->find('FOOD');
+
+        $product = new Product();
+        $product->id = '1';
+        $product->name = 'Product 1';
+        $product->description = 'Description 1';
+
+        $category->products()->save($product);
+
+        self::assertNotNull($product->category_id);
+    }
+
+    public function testRelationshipQuery()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $category = Category::query()->find('FOOD');
+        $products = $category->products;
+        self::assertCount(1, $products);
+
+        $outOfStockProducts = $category->products()->where('stock', '<=', '0')->get();
+        self::assertCount(1, $outOfStockProducts);
+        var_dump($outOfStockProducts[0]->description);
+
+    }
+
 }
--- belajar-laravel-eloquent/tests/Feature/CustomerTest.php
+++ belajar-laravel-eloquent/tests/Feature/CustomerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Customer;
+use App\Models\Wallet;
 use Database\Seeders\CustomerSeeder;
 use Database\Seeders\WalletSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -22,5 +23,20 @@ class CustomerTest extends TestCase
         self::assertNotNull($wallet);
 
         self::assertEquals(1_000_000, $wallet->amount);
+    }
+
+    public function testOneToOneQuery()
+    {
+        $customer = new Customer();
+        $customer->id = 'ADE';
+        $customer->name = 'Ade';
+        $customer->email = 'ade@pzn.com';
+        $customer->save();
+
+        $wallet = new Wallet();
+        $wallet->amount = 1_000_000;
+        $customer->wallet()->save($wallet);
+
+        self::assertNotNull($wallet->customer_id);
     }
 }
