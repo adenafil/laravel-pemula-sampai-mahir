8c79ee93c8009ff9a7447af0eb50360376342218
adenafil <nafilie9@gmail.com>
2024-05-04 15:07:13 +0700

    216. Eloquent Collection

--- belajar-laravel-eloquent/tests/Feature/ProductTest.php
+++ belajar-laravel-eloquent/tests/Feature/ProductTest.php
@@ -118,4 +118,18 @@ class ProductTest extends TestCase
         }
     }
 
+    public function testEloquentCollection()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        // 2 Products, 1, 2
+        $products = Product::get();
+
+        // where id in (1, 2);
+        $products = $products->toQuery()->where('price', 200)->get();
+
+        self::assertNotNull($products);
+        self::assertEquals(2, $products[0]->id);
+
+    }
 }
