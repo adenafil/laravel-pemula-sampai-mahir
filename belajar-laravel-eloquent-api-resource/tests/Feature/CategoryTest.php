1c03939156ee257051b4d07057584b3f12f44fd2
adenafil <nafilie9@gmail.com>
2024-05-11 20:12:06 +0700

    252. Resource

---
+++ belajar-laravel-eloquent-api-resource/tests/Feature/CategoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use Database\Seeders\CategorySeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    public function testResource()
+    {
+        $this->seed([CategorySeeder::class]);
+
+        $category = Category::first();
+
+        $this->get("/api/categories/$category->id")
+            ->assertStatus(200)
+            ->assertJson([
+               'data' => [
+                   'id' => $category->id,
+                   'name' => $category->name,
+                   'created_at' => $category->created_at->toJson(),
+                   'updated_at' => $category->updated_at->toJson(),
+               ]
+            ]);
+    }
+}
