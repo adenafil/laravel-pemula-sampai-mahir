ace4b2b183a0c5069b94b4040b05e30b4212b1dd
adenafil <nafilie9@gmail.com>
2024-06-03 21:47:06 +0700

    311. Authentication

---
+++ belajar-laravel-security/belajar-laravel-security/tests/Feature/UserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Database\Seeders\UserSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testAuth()
+    {
+        $this->seed([UserSeeder::class]);
+        $success = Auth::attempt([
+            'email' => 'ade@localhost',
+            'password' => 'rahasia123'
+        ], true);
+
+        self::assertTrue($success);
+
+        $user = Auth::user();
+
+        self::assertNotNull($user);
+        self::assertEquals('ade@localhost', $user->email);
+    }
+
+    public function testGuest()
+    {
+        $user = Auth::user();
+        self::assertNull($user);
+    }
+}
